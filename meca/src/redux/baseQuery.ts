8b9986ddbc2513e680e63dbddc6ca8ac5de6b97f
Hunchojoe19 <enubiakjoseph@gmail.com>
2024-04-17 14:51:51 +0100

    created project that consist of new dependencies

---
+++ meca/src/redux/baseQuery.ts
@@ -0,0 +1,17 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { getSession } from "next-auth/react";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export const baseQueryAuth = fetchBaseQuery({
+  baseUrl,
+  prepareHeaders: async (headers) => {
+    const session = await getSession();
+    const token = session?.user?.access_token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+      headers.set("Content-Type", `application/json`);
+    }
+    return headers;
+  },
+});
