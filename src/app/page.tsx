3b3a140b3b3bb3373a0baf77b325085c03666a61
blessingayo <asuelimenblessing630@gmail.com>
2024-05-02 20:43:16 +0100

    home page category dropdown almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-import { Header } from "./reusables/header/page";
-import { NavBar } from "./reusables/NavBar/page";
+import { Header } from "@/app/reusables/Header/page";
+import { NavBar } from "@/app/reusables/NavBar/page";
 
 export default function Home() {
   // const handleClick = () => {
