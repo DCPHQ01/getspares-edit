42b26efff3be9414b08cc35b0edd50e676492e39
Ashley <ashleyndabai@gmail.com>
2024-06-24 11:02:24 +0100

    checkout and order

--- src/app/dashboard/actors/buyer/buyerMobileDashboard/BuyerOrderMobile.tsx
+++ src/app/dashboard/actors/buyer/buyerMobileDashboard/BuyerOrderMobile.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../../components/ui/header";
 import Searchbox from "../../../components/ui/searchbox";
 import OrderTable from "../../../components/table/buyerAdmin/orderTable";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useGetViewAllOrdersQuery } from "../../../../../redux/features/dashboard/buyerQuery";
 
 function BuyerOrderMobile() {
+  const [pageNo, setPageNo] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const {data, isLoading} = useGetViewAllOrdersQuery({pageNo, pageSize});
+  console.log("data for orders", data);
+
+  const [buyerOrderList, setBuyerOrderList] = useState([]);
+  useEffect(() => {
+    if (data) {
+      console.log("Received data structure:", data);
+      const resultList = data?.data?.content;
+      if (resultList) {
+        setBuyerOrderList(resultList);
+      } else {
+        console.error("Failed to show buyer order", resultList);
+      }
+    }
+  }, [data]);
+  console.log("The buyerOrderList:", buyerOrderList);
   return (
     <div className={`flex flex-col gap-6`}>
       <Header
@@ -14,7 +33,7 @@ function BuyerOrderMobile() {
       />
       <Searchbox />
 
-      <OrderTable />
+      <OrderTable data={buyerOrderList} isLoading={isLoading}/>
 
       <div className=" flex justify-end mt-10 mb-10 font-bold text-lg">
         {/* <button className="flex gap-x-2 border border-[#EAECF0]  rounded-md h-[36px] w-[36px] pl-1">
