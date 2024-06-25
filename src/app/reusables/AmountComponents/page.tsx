0dca196617a43bb0ab99b94824a935216271c53b
blessingayo <asuelimenblessing630@gmail.com>
2024-06-25 18:45:46 +0100

    ... pulled code

--- src/app/reusables/AmountComponents/page.tsx
+++ src/app/reusables/AmountComponents/page.tsx
@@ -1,14 +1,11 @@
-"use client"
-
-const formatNumber = (value: number): string => {
-  return new Intl.NumberFormat("en-US", {
-    style: "decimal",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(value);
-};
-
 const AmountComponentsPage: React.FC<{ amount: number }> = ({ amount }) => {
+  const formatNumber = (value: number): string => {
+    return new Intl.NumberFormat("en-US", {
+      style: "decimal",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(value);
+  };
   return <div>{formatNumber(amount)}</div>;
 };
 
