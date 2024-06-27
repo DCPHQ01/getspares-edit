07c1357b689fce1d5f9f05259745331253195271
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-06-27 20:29:45 +0100

    checking for error while building

--- src/app/(dashboard)/admin/Buyers.tsx
+++ src/app/(dashboard)/admin/Buyers.tsx
@@ -41,11 +41,7 @@ function Buyers() {
     if (currentPage * size < totalElement) {
       setCurrentPage((prevPage) => prevPage + 1);
     }
-
-
-  
-    
-
+  }
  
   return (
     <>
@@ -78,5 +74,5 @@ function Buyers() {
       </div>
     </>
   );
-}
+} 
 export default Buyers;
