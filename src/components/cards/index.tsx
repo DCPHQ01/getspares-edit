1947ab9857d91bc3af23ff364b1861037c681fbf
ikjames5454 <ikennajames03@gmail.com>
2024-06-27 12:11:44 +0100

    view

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -4,13 +4,10 @@ import { paths } from "../../path/paths";
 import { sidePanel } from "../../app/dashboard/components/utils/utils";
 
 interface CardProp {
-  totalNumberOfAgents?: number
-  totalNumberOfProductsSold?: number
-  totalOrderValue?: number
-  totalNumberOfPartOrdered?: number,
-  totalNumberOfAgent?: number,
-  totalTransactionValue?: number,
-  totalNumberOfVendor?: number,
+  totalNumberOfPartOrdered: number,
+  totalNumberOfAgent: number,
+  totalTransactionValue: number,
+  totalNumberOfVendor: number,
 }
 
 interface CardProps {
@@ -20,27 +17,16 @@ interface CardProps {
 const Index:React.FC<CardProps> = ({cardField}) => {
   const cardProps = [
     {
-<<<<<<< HEAD
       total: "Total number of parts ordered",
       amount: cardField.totalNumberOfPartOrdered,
-=======
-      total: "number of parts ordered",
-      amount: cardField.totalNumberOfPartOrdered || 0,
->>>>>>> 40245a68be0518195005c0a7aa08e73b7af0a0ac
       percentage: 32,
       onClick: () => {
         console.log("View total number of parts ordered");
       },
     },
     {
-<<<<<<< HEAD
       total: "Total number of agents",
-      amount: cardField.totalNumberOfAgent || cardField.totalNumberOfAgents,
-=======
-      total: "number of agents",
-
-      amount: cardField.totalNumberOfAgent || cardField.totalNumberOfAgents || 0,
->>>>>>> 40245a68be0518195005c0a7aa08e73b7af0a0ac
+      amount: cardField.totalNumberOfAgent ,
       percentage: 10,
 
       onClick: () => {
@@ -48,26 +34,16 @@ const Index:React.FC<CardProps> = ({cardField}) => {
       },
     },
     {
-<<<<<<< HEAD
       total: "Total transaction value",
-      amount: cardField.totalTransactionValue || cardField.totalOrderValue,
-=======
-      total: "transaction value",
-      amount: cardField.totalTransactionValue || cardField.totalOrderValue || 0,
->>>>>>> 40245a68be0518195005c0a7aa08e73b7af0a0ac
+      amount: cardField.totalTransactionValue ,
       percentage: 59,
       onClick: () => {
         console.log("View total transaction value");
       },
     },
     {
-<<<<<<< HEAD
       total: "Total number of Product sold",
-      amount: cardField.totalNumberOfVendor || cardField.totalNumberOfProductsSold,
-=======
-      total: "total number of Product sold",
-      amount: cardField.totalNumberOfVendor || cardField.totalNumberOfProductsSold || 0,
->>>>>>> 40245a68be0518195005c0a7aa08e73b7af0a0ac
+      amount: cardField.totalNumberOfVendor ,
       percentage: 43,
       onClick: () => {
         console.log("View total number of vendors");
