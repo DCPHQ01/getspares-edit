4aae3d0ce0fae89e4780d4a4f587c5dc234f26be
blessingayo <asuelimenblessing630@gmail.com>
2024-06-28 09:34:36 +0100

    fixed the store bug

--- src/app/dashboard/components/table/mecaadmin/agentTable.tsx
+++ src/app/dashboard/components/table/mecaadmin/agentTable.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React from "react";
 import styles from "../styles.module.css";
-import Image from "next/image";
-import Stack from "@mui/material/Stack";
 import dayjs from "dayjs";
-import { AccountCircle } from '@mui/icons-material';
+import { AccountCircle } from "@mui/icons-material";
 import { ColorRing } from "react-loader-spinner";
 
 interface Agent {
@@ -22,12 +20,8 @@ interface AgentTableProps {
 }
 
 const formatDateTime = (dateTime: string) => {
-
-  const date = dayjs(dateTime).format("YYYY-MM-DD");
-  const time = dayjs(dateTime).format("HH:mm a");
   const date = dayjs(dateTime).format("DD-MM-YYYY");
-  const time = dayjs(dateTime).format("hh:mm A"); 
-
+  const time = dayjs(dateTime).format("hh:mm A");
   return { date, time };
 };
 
@@ -54,12 +48,9 @@ const AgentTable: React.FC<AgentTableProps> = ({ agentList, isLoading }) => {
               </tr>
             </thead>
             <tbody>
-
-              {isLoading? ( 
-                <div 
-                className="text-center mt-28 relative lg:left-[210%] lg:right[210%] md:left-[213%] md:right[213%] sm:left-[21"
-                >
-                   <ColorRing  
+              {isLoading ? (
+                <div className="text-center mt-28 relative lg:left-[210%] lg:right[210%] md:left-[213%] md:right[213%] sm:left-[21">
+                  <ColorRing
                     visible={true}
                     height="40"
                     width="40"
@@ -80,7 +71,6 @@ const AgentTable: React.FC<AgentTableProps> = ({ agentList, isLoading }) => {
                   />
                   <p>Loading Agent........</p>
                 </div>
-
               ) : (
                 agentList?.map((d, index) => {
                   const { date, time } = formatDateTime(d.dateAdded);
@@ -95,18 +85,6 @@ const AgentTable: React.FC<AgentTableProps> = ({ agentList, isLoading }) => {
                           className={`flex gap-3 text-[0.88rem] py-[1rem] px-[1.25rem]`}
                         >
                           <div id={`companyDetails_${index}`}>
-                            {/* {d.imageUrl ? (
-                        <Image
-                          src={d.imageUrl}
-                          className="object-contain"
-                          alt="Avatar"
-                          id={`avatar_${index}`}
-                        />
-                      ) : (
-                        <AccountCircle style={{ fontSize: 50 }} className=" text-gray-400" 
-                        style={{ fontSize:40, color: gray}}
-                        />
-                      )} */}
                             <div id={`companyDetails_${index}`}>
                               <div className="truncate">{d.firstName}</div>
                               <div
@@ -117,27 +95,6 @@ const AgentTable: React.FC<AgentTableProps> = ({ agentList, isLoading }) => {
                               </div>
                             </div>
                           </div>
-                          {/* {d.imageUrl ? (
-                        <Image
-                          src={d.imageUrl}
-                          className="object-contain"
-                          alt="Avatar"
-                          width={50}
-                          height={50}
-                        />
-                      ) : (
-                        <AccountCircle style={{ fontSize: 50 }} className="text-gray-400"
-                        style={{ fontSize:40, color: gray}}
-                         />
-                      )}
-                          <div className="truncate">{d.firstName}</div>
-                          <div
-                            className={`text-[#4B5565] truncate`}
-                            id={`email_${index}`}
-                          >
-                            {d.email}
-                          </div>
-                        </div> */}
                         </div>
                       </td>
                       <td
@@ -153,49 +110,6 @@ const AgentTable: React.FC<AgentTableProps> = ({ agentList, isLoading }) => {
                         {d.transactionValue}
                       </td>
                       <td id={`dateJoined_${index}`}>
-
-              ) :(
-                 agentList?.map((d, index) => {
-                const { date, time } = formatDateTime(d.dateAdded);
-                return (
-                  <tr
-                    key={index}
-                    id={`row_${index}`}
-                    className="cursor-pointer hover:bg-gray-50"
-                  >
-                    <td id={`companyData_${index}`}>
-                      <div
-                        className={`flex gap-3 text-[0.88rem] py-[1rem] px-[1.25rem]`}
-                      >
-                        <div id={`companyDetails_${index}`}>
-                        <div id={`companyDetails_${index}`}>
-                        <div className="truncate">{d.firstName}</div>
-                        <div
-                          className={`text-[#4B5565] truncate`}
-                          id={`email_${index}`}
-                        >
-                          {d.email}
-                        </div>
-                      </div>
-                    </div>
-                      </div>
-                    </td>
-                    <td
-                      className={`text-[0.88rem] py-[1rem] px-[3.13rem]`}
-                      id={`itemsSold_${index}`}
-                    >
-                      {d.quantitySold}
-                    </td>
-                    <td
-                      className={`text-[0.88rem] py-[1rem] px-[3.13rem]`}
-                      id={`transactionValue_${index}`}
-                    >
-                      {d.transactionValue}
-                    </td>
-                    <td id={`dateJoined_${index}`}>
-                      <div className={`text-[0.88rem] py-[1rem] px-[2.75rem]`}>
-                        <div id={`date_${index}`}>{date}</div>
-
                         <div
                           className={`text-[0.88rem] py-[1rem] px-[2.75rem]`}
                         >
