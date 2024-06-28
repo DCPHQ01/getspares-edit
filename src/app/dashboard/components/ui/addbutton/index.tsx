d063639b4b3129e0b7029b6a51428b517a60263d
Hunchojoe19 <enubiakjoseph@gmail.com>
2024-06-28 10:37:59 +0100

    pull

--- src/app/dashboard/components/ui/addbutton/index.tsx
+++ src/app/dashboard/components/ui/addbutton/index.tsx
@@ -8,13 +8,13 @@ function Index({ title, long }: IProps) {
     <>
       <button
         id="addButton"
-        className={`bg-[#095AD3] lg:w-[250px] w-[100%] text-white rounded-full py-[0.58rem] px-[1.5rem] 
+        className={`bg-[#095AD3] lg:w-[180px] w-[100%] text-white rounded-full py-2 px-2 
         `}
       >
         <div className={`flex text-white items-center justify-center`}>
           <MdAdd size={18} />
 
-          <span className="ml-1">  {title}</span>
+          <span className="ml-1"> {title}</span>
         </div>
       </button>
     </>
