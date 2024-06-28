d0b53545b1aedda59723e9f56941c950b21c51b5
Hunchojoe19 <enubiakjoseph@gmail.com>
2024-06-28 16:00:16 +0100

    pushed

--- src/app/category/products/[name]/page.tsx
+++ src/app/category/products/[name]/page.tsx
@@ -4,7 +4,7 @@ import part from "../../../../assets/images/parts.png";
 import star from "../../../../assets/images/Star.png";
 import Image from "next/image";
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useLayoutEffect, useState } from "react";
 
 import {
   Accordion,
@@ -31,70 +31,12 @@ import { ColorRing } from "react-loader-spinner";
 
 interface ItemsDataProps {
   id: number;
+  name: string;
   desc: string;
   rating: number;
   price: string;
   image?: any;
 }
-
-const itemsData: ItemsDataProps[] = [
-  {
-    id: 1,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 2,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 3,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 4,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 5,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 6,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 7,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-  {
-    id: 8,
-    desc: "E46 Engine 1996 Model",
-    rating: 3,
-    price: "₦97,500.00",
-    image: part,
-  },
-];
 
 export default function Products() {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
@@ -120,18 +62,14 @@ export default function Products() {
     router.push(`/category/products/${searches}/${id}`);
   };
 
-  const { data, isLoading } = useGetProductInCategoryQuery({
+  const { data, isFetching } = useGetProductInCategoryQuery({
     categoryId,
     pageNumber: 0,
     pageSize: 100,
   });
-  console.log("data for categories ", data?.data?.content);
-  useEffect(() => {
-    const storedCategoryId = sessionStorage.getItem("categoryId");
-    if (storedCategoryId) {
-      setCategoryId(storedCategoryId);
-    }
-  }, []);
+  console.log("data for categories: ", data);
+  console.log("category id: ", categoryId);
+
   const filterData = [
     {
       id: 1,
@@ -159,29 +97,9 @@ export default function Products() {
         },
       ],
     },
+
     {
       id: 2,
-      title: "Model",
-      items: [
-        {
-          id: 1,
-          title: "New",
-          icon: <Checkbox />,
-        },
-        {
-          id: 2,
-          title: "Refurbished",
-          icon: <Checkbox />,
-        },
-        {
-          id: 3,
-          title: "Not specified",
-          icon: <Checkbox />,
-        },
-      ],
-    },
-    {
-      id: 3,
       title: "Conditions",
       items: [
         {
@@ -202,7 +120,7 @@ export default function Products() {
       ],
     },
     {
-      id: 4,
+      id: 3,
       title: "Price",
       items: [
         {
@@ -278,7 +196,7 @@ export default function Products() {
                   className="mt-4 w-full flex items-center flex-wrap gap-x-5"
                   id="allItemsContainerDiv"
                 >
-                  {isLoading ? (
+                  {isFetching ? (
                     <div className="w-full h-screen flex justify-center items-center">
                       <ColorRing
                         visible={true}
@@ -327,7 +245,7 @@ export default function Products() {
                           id="itemContentDiv"
                         >
                           <div className="flex justify-between items-center md:hidden">
-                            <TruncateText text={item.desc} maxLength={15} />
+                            <TruncateText text={item.name} maxLength={15} />
                             <div
                               id="ratingContainerMobile"
                               className="flex items-center gap-x-1"
@@ -388,7 +306,10 @@ export default function Products() {
             className="flex items-center gap-2 mt-56 px-8"
             id="breadCrumbsDivDesktop"
           >
-            <p className="font-nunito text-sm font-medium text-mecaDarkBlueBackgroundOverlay">
+            <p
+              className="font-nunito cursor-pointer text-sm font-medium text-mecaDarkBlueBackgroundOverlay"
+              onClick={() => router.push("/")}
+            >
               Home
             </p>
             <MdChevronRight size={20} />
@@ -403,90 +324,94 @@ export default function Products() {
             <p className="text-2xl text-mecaDarkBlueBackgroundOverlay font-bold font-nunito">
               {searchWords}
             </p>
-            <div className="flex gap-x-2 items-center" id="toggler/header">
-              <p className="text-sm text-mecaDarkBlueBackgroundOverlay font-normal font-nunito">
-                Hide filters
-              </p>
-              <Switches onClick={handleToggleFilter} />
-            </div>
+            {data?.data?.content?.length > 0 && (
+              <div className="flex gap-x-2 items-center" id="toggler/header">
+                <p className="text-sm text-mecaDarkBlueBackgroundOverlay font-normal font-nunito">
+                  Hide filters
+                </p>
+                <Switches onClick={handleToggleFilter} />
+              </div>
+            )}
           </div>
           <div
             className="mt-8 px-8 flex gap-x-4 justify-between"
             id="filterDivForDesktopContainer"
           >
-            <div
-              className={`flex flex-col ${showFilter ? "w-[28%]" : "hidden"}`}
-              id="filterSideBarContainer"
-            >
-              <p className="text-lg font-medium text-nunito text-mecaDarkBlueBackgroundOverlay">
-                Filter by
-              </p>
-              {filterData.map((data) => (
-                <div className="-ml-4" id="navDatum" key={data.id}>
-                  <Accordion
-                    defaultExpanded
-                    className="w-full"
-                    style={{ boxShadow: "none" }}
-                  >
-                    <AccordionSummary
-                      expandIcon={<MdExpandMore size={28} />}
-                      aria-controls="panel1a-content"
-                      id="panel1a-header"
+            {data?.data?.content?.length > 0 && (
+              <div
+                className={`flex flex-col ${showFilter ? "w-[28%]" : "hidden"}`}
+                id="filterSideBarContainer"
+              >
+                <p className="text-lg font-medium text-nunito text-mecaDarkBlueBackgroundOverlay">
+                  Filter by
+                </p>
+                {filterData.map((data) => (
+                  <div className="-ml-4" id="navDatum" key={data.id}>
+                    <Accordion
+                      defaultExpanded
+                      className="w-full"
+                      style={{ boxShadow: "none" }}
                     >
-                      <p className="text-mecaGoBackText text-[16px] font-semibold font-nunito capitalize">
-                        {data.title}
-                      </p>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <div className="flex flex-col gap-y-4">
-                        <FormControl>
-                          <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            name="radio-buttons-group"
-                            defaultValue={data.items[0].title}
-                          >
-                            {data.items.map((item: any) => (
-                              <div
-                                className="flex items-center gap-4"
-                                key={item.id}
-                                id="checkboxItemList"
-                              >
-                                <FormControlLabel
-                                  className="text-mecaGoBackText text-sm font-nunito"
-                                  control={item.icon}
-                                  label={`${item.title}`}
-                                />
-                              </div>
-                            ))}
-                          </RadioGroup>
-                        </FormControl>
-                      </div>
-                    </AccordionDetails>
-                  </Accordion>
+                      <AccordionSummary
+                        expandIcon={<MdExpandMore size={28} />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                      >
+                        <p className="text-mecaGoBackText text-[16px] font-semibold font-nunito capitalize">
+                          {data.title}
+                        </p>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <div className="flex flex-col gap-y-4">
+                          <FormControl>
+                            <RadioGroup
+                              aria-labelledby="demo-radio-buttons-group-label"
+                              name="radio-buttons-group"
+                              defaultValue={data.items[0].title}
+                            >
+                              {data.items.map((item: any) => (
+                                <div
+                                  className="flex items-center gap-4"
+                                  key={item.id}
+                                  id="checkboxItemList"
+                                >
+                                  <FormControlLabel
+                                    className="text-mecaGoBackText text-sm font-nunito"
+                                    control={item.icon}
+                                    label={`${item.title}`}
+                                  />
+                                </div>
+                              ))}
+                            </RadioGroup>
+                          </FormControl>
+                        </div>
+                      </AccordionDetails>
+                    </Accordion>
+                  </div>
+                ))}
+                <div className="mt-8 flex flex-col justify-center gap-y-4 w-[273px] h-[100px]">
+                  <button
+                    type="button"
+                    className="w-full h-[48px] bg-mecaBluePrimaryColor text-white font-nunito font-semibold text-sm rounded-full"
+                    id="applyFilterButton"
+                  >
+                    Apply Filter
+                  </button>
+                  <button
+                    type="button"
+                    className="w-full h-[48px] border border-mecaBluePrimaryColor text-mecaBluePrimaryColor font-nunito font-semibold text-sm rounded-full"
+                    id="clearFilterButton"
+                  >
+                    Cancel Filter
+                  </button>
                 </div>
-              ))}
-              <div className="mt-8 flex flex-col justify-center gap-y-4 w-[273px] h-[100px]">
-                <button
-                  type="button"
-                  className="w-full h-[48px] bg-mecaBluePrimaryColor text-white font-nunito font-semibold text-sm rounded-full"
-                  id="applyFilterButton"
-                >
-                  Apply Filter
-                </button>
-                <button
-                  type="button"
-                  className="w-full h-[48px] border border-mecaBluePrimaryColor text-mecaBluePrimaryColor font-nunito font-semibold text-sm rounded-full"
-                  id="clearFilterButton"
-                >
-                  Cancel Filter
-                </button>
               </div>
-            </div>
+            )}
             <div
               className="flex flex-wrap justify-between w-full"
               id="allItemsContainerDivDesktop"
             >
-              {isLoading ? (
+              {isFetching ? (
                 <div className="w-full h-screen flex justify-center items-center">
                   <ColorRing
                     visible={true}
@@ -535,7 +460,7 @@ export default function Products() {
                       id="itemContent"
                     >
                       <div className="flex justify-between items-center">
-                        <TruncateText text={item.desc} maxLength={50} />
+                        <TruncateText text={item.name} maxLength={50} />
                         <div
                           id="ratingContainerDesktop"
                           className="flex items-center gap-x-1"
