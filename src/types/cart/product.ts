2fd786b369d2ec55e8fccf4d956f601948b4aa4a
Hunchojoe19 <enubiakjoseph@gmail.com>
2024-07-05 15:29:08 +0100

    added changes

--- src/types/cart/product.ts
+++ src/types/cart/product.ts
@@ -1,29 +1,29 @@
 export type CartProduct = {
-   availabilityStatus: string;
-   category: string;
-   company: string;
-   companyId: null;
-   description: string;
-   id: string;
-   images: [];
-   name: string;
-   amount: string;
-   currency: string;
-   quantity:string;
-   productInformation:{
-      brand: string;
-      countryOfOrigin: string;
-      itemModelNumber: string;
-      itemWeight: string;
-      manufacturer: string;
-      manufacturerPartNumber: string;
-      model: string;
-      productionDimension: string;
-      voltage: string;
-      quantity:number;
-   };
-}
+  availabilityStatus: string;
+  category: string;
+  company: string;
+  companyId: null;
+  description: string;
+  id: string;
+  images: string[];
+  name: string;
+  amount: string;
+  currency: string;
+  quantity: string;
+  productInformation: {
+    brand: string;
+    countryOfOrigin: string;
+    itemModelNumber: string;
+    itemWeight: string;
+    manufacturer: string;
+    manufacturerPartNumber: string;
+    model: string;
+    productionDimension: string;
+    voltage: string;
+    quantity: number;
+  };
+};
 
 export type CartState = {
-   cart: CartProduct[];
+  cart: CartProduct[];
 };
