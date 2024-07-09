b6aca40becf955f4acabe1d7d95d9ebf822c6095
Ashley <ashleyndabai@gmail.com>
2024-07-09 10:22:05 +0100

    worked on add cateory

--- src/app/addProductDashboard/addImages/page.tsx
+++ src/app/addProductDashboard/addImages/page.tsx
@@ -142,7 +142,10 @@ const CalledPagesPageTwoPages = () => {
     if (savedImages) {
       setImagesUrl(JSON.parse(savedImages));
     }
-  }, []);
+    else{
+          setImagesUrl(productImage?.images);
+        }
+  }, [productImage]);
   // useEffect(() => {
   //   if (productImage) {
   //     setImagesUrl(productImage?.images);
