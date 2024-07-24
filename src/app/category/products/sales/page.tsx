352b918513bb032389fe3ded62ed9ef965774b04
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-07-24 12:20:11 +0100

    commit to switch into new branch

--- src/app/category/products/sales/page.tsx
+++ src/app/category/products/sales/page.tsx
@@ -8,12 +8,13 @@ import NavBar from "../../../../components/NavBar/NavBar";
 import TopBarWhileInside from "../../../reusables/TopBarWhileInside/page";
 import sellerImage from "../../../../assets/images/sellerImage.png";
 import emailIcon from "../../../../assets/icons/emailIcon.png";
-import { MdChevronRight } from "react-icons/md";
 import Card from "../../../../components/Homepage/Card";
 import HomeImage2 from "../../../../assets/images/homeImage1.png";
 import { ColorRing } from "react-loader-spinner";
 import { useSearchParams } from "next/navigation";
 import { useGetAllSalesQuery } from "../../../../redux/features/product/productsQuery";
+import { MdChevronRight, MdExpandMore } from "react-icons/md";
+
 interface productDetails {
   id: string;
   name: string;
@@ -45,79 +46,90 @@ const Sales = () => {
   }, [data]);
 
   return (
-    <div>
-      <TopBarWhileInside />
-      {isFetching ? (
-        <div className="w-full h-[615px] flex justify-center items-center">
-          <ColorRing
-            visible={true}
-            height="100"
-            width="100"
-            ariaLabel="color-ring-loading"
-            wrapperStyle={{}}
-            wrapperClass="color-ring-wrapper"
-            colors={["#095AD3", "#095AD3", "#095AD3", "#095AD3", "#095AD3"]}
-          />
-        </div>
-      ) : (
-        <>
-          <div className="px-12">
-            <div className="h-80 w-full mt-48 bg-blue-100">
-              <div className="flex flex-col pl-10 pt-10">
-                {typeof companyImg !== "string" && companyImg !== "" ? (
-                  <img
-                    src={companyImg}
-                    width="50"
-                    height="50"
-                    alt="image"
-                    className="rounded-full object-cover"
-                  />
-                ) : (
-                  <AccountCircle
-                    style={{ fontSize: 100 }}
-                    className="text-gray-400"
-                  />
-                )}
-                <div className="flex flex-col justify-start pt-5">
-                  <p className="font-bold text-2xl">{name}</p>
-                  <p className="mt-5 text-gray-500 text-xl">{email}</p>
-                  <div className="flex gap-4">
-                    <div className="mt-3">
-                      <Image src={emailIcon} alt="email icon" />
+      <div>
+          <TopBarWhileInside/>
+        <div className={``}>
+          <div className={``}>
+            <p className="mt-12 font-nunito text-sm font-medium text-mecaDarkBlueBackgroundOverlay">
+              Home
+            </p>
+            <MdChevronRight size={20}/>
+            <p className="font-nunito text-sm font-medium text-mecaGoBackArrow capitalize">
+              {/*{searchWords}*/}
+            </p>
+          </div>
+          {isFetching ? (
+              <div className="w-full h-[615px] flex justify-center items-center">
+                <ColorRing
+                    visible={true}
+                    height="100"
+                    width="100"
+                    ariaLabel="color-ring-loading"
+                    wrapperStyle={{}}
+                  wrapperClass="color-ring-wrapper"
+                  colors={["#095AD3", "#095AD3", "#095AD3", "#095AD3", "#095AD3"]}
+              />
+            </div>
+        ) : (
+            <>
+              <div className="px-12">
+                <div className="h-80 w-full mt-48 bg-blue-100">
+                  <div className="flex flex-col pl-10 pt-10">
+                    {typeof companyImg !== "string" && companyImg !== "" ? (
+                        <img
+                            src={companyImg}
+                            width="50"
+                            height="50"
+                            alt="image"
+                            className="rounded-full object-cover"
+                        />
+                    ) : (
+                        <AccountCircle
+                            style={{fontSize: 100}}
+                            className="text-gray-400"
+                        />
+                    )}
+                    <div className="flex flex-col justify-start pt-5">
+                      <p className="font-bold text-2xl">{name}</p>
+                      <p className="mt-5 text-gray-500 text-xl">{email}</p>
+                      <div className="flex gap-4">
+                        <div className="mt-3">
+                          <Image src={emailIcon} alt="email icon"/>
+                        </div>
+                        <p className="mt-2 text-mecaBluePrimaryColor text-lg">
+                          {websiteUrl}
+                        </p>
+                      </div>
                     </div>
-                    <p className="mt-2 text-mecaBluePrimaryColor text-lg">
-                      {websiteUrl}
-                    </p>
                   </div>
                 </div>
+                <p className="flex justify-start pt-10 text-3xl">Inventory</p>
               </div>
-            </div>
-            <p className="flex justify-start pt-10 text-3xl">Inventory</p>
-          </div>
-          <div className="flex flex-wrap -mx-2 pt-10">
-            {list?.map((product: productDetails) => (
-              <div key={product.id} className="w-full sm:w-1/2 md:w-1/3 p-2">
-                <Card
-                  id={product.id}
-                  productName={product.name}
-                  price={product.price}
-                  productImage={product.image}
-                  rating={product.rating}
-                  image={HomeImage2}
-                />
+              <div className="flex flex-wrap -mx-2 pt-10">
+                {list?.map((product: productDetails) => (
+                    <div key={product.id} className="w-full sm:w-1/2 md:w-1/3 p-2">
+                      <Card
+                          id={product.id}
+                          productName={product.name}
+                          price={product.price}
+                          productImage={product.image}
+                          rating={product.rating}
+                          image={HomeImage2}
+                      />
+                    </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
+            </>
+        )}
+        </div>
+      </div>
   );
 };
 
 const MyComponentWithSuspense = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Sales />
-  </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Sales/>
+    </Suspense>
 );
 
 export default MyComponentWithSuspense;
